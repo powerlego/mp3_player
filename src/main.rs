a166a9a32f0e55c99771de26610c82b9f7095952
powerlego <11838540+powerlego@users.noreply.github.com>
2022-12-18 14:14:00 -0600

    updated to gtk4 and updated menubar

--- src/main.rs
+++ src/main.rs
@@ -1,226 +1,64 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, HeaderBar};
+use gtk::{Application, ApplicationWindow, gio, glib};
 
-/// Build the File menu
-/// # Arguments
-/// * `app` - The application
-/// # Returns
-/// The File menu
-/// # Example
-/// ```
-/// let file_menu = build_file_menu(app);
-/// ```
-/// # Safety
-/// This function is unsafe because it calls the `connect_activate` method on the menu items which is unsafe.
-/// # Panic
-/// This function will panic if the `connect_activate` method panics.
-fn build_file_menu(app: Application) -> gtk::Menu {
-    let menu = gtk::Menu::new();
+fn build_menu(app: &gtk::Application) {
+    let menu = gio::Menu::new();
+    let file_menu = gio::Menu::new();
+    file_menu.append(Some("Open"), Some("app.open"));
+    file_menu.append(Some("Quit"), Some("app.quit"));
+    menu.append_submenu(Some("File"), &file_menu);
+    app.set_menubar(Some(&menu));
+}
 
-    /* -------------------------------------------------------------------------- */
-    /*                                New Playlist                                */
-    /* -------------------------------------------------------------------------- */
-    let new_playlist = gtk::MenuItem::builder()
-        .label("New Playlist")
-        .accel_path("<MP3Player>/File/New Playlist")
-        .build();
-    new_playlist.connect_activate(|_| {
-        println!("New Playlist");
-    });
-    menu.append(&new_playlist);
+fn add_accelerators(app: &gtk::Application) {
+    app.set_accels_for_action("app.open", &["<Primary>O"]);
+    app.set_accels_for_action("app.quit", &["<Primary>Q"]);
+}
 
-    /* -------------------------------------------------------------------------- */
-    /*                                    Open                                    */
-    /* -------------------------------------------------------------------------- */
-    let open = gtk::MenuItem::builder()
-        .label("Open")
-        .accel_path("<MP3Player>/File/Open")
-        .build();
-    open.connect_activate(|_| {
+fn add_actions(app: &gtk::Application, window: &gtk::ApplicationWindow) {
+    let open_action = gio::SimpleAction::new("open", None);
+    open_action.connect_activate(|_, _| {
         println!("Open");
     });
-    menu.append(&open);
-
-    /* -------------------------------------------------------------------------- */
-    /*                                 Separator 1                                */
-    /* -------------------------------------------------------------------------- */
-    let separator = gtk::SeparatorMenuItem::new();
-    menu.append(&separator);
-
-    /* -------------------------------------------------------------------------- */
-    /*                                   Import                                   */
-    /* -------------------------------------------------------------------------- */
-    let import = gtk::Menu::new();
-
-    /* ------------------------------- Import File ------------------------------ */
-    let import_file = gtk::MenuItem::builder()
-        .label("File")
-        .accel_path("<MP3Player>/File/Import/File")
-        .build();
-    import_file.connect_activate(|_| {
-        println!("Import File");
-    });
-    import.append(&import_file);
-
-    /* ------------------------------- Import Folder ------------------------------ */
-    let import_folder = gtk::MenuItem::builder()
-        .label("Folder")
-        .accel_path("<MP3Player>/File/Import/Folder")
-        .build();
-    import_folder.connect_activate(|_| {
-        println!("Import Folder");
-    });
-    import.append(&import_folder);
-
-    /* ------------------------------- Import Playlist ------------------------------ */
-    let import_playlist = gtk::MenuItem::builder()
-        .label("Playlist")
-        .accel_path("<MP3Player>/File/Import/Playlist")
-        .build();
-    import_playlist.connect_activate(|_| {
-        println!("Import Playlist");
-    });
-    import.append(&import_playlist);
-
-    /* ------------------------------- Import Item ------------------------------ */
-    let import_item = gtk::MenuItem::builder()
-        .label("Import")
-        .accel_path("<MP3Player>/File/Import")
-        .submenu(&import)
-        .build();
-    menu.append(&import_item);
-
-    /* -------------------------------------------------------------------------- */
-    /*                                   Export                                   */
-    /* -------------------------------------------------------------------------- */
-    let export = gtk::MenuItem::builder()
-        .label("Export Playlist")
-        .accel_path("<MP3Player>/File/Export Playlist")
-        .build();
-    export.connect_activate(|_| {
-        println!("Export Playlist");
-    });
-    menu.append(&export);
-
-    /* -------------------------------------------------------------------------- */
-    /*                                 Separator 2                                */
-    /* -------------------------------------------------------------------------- */
-    let separator = gtk::SeparatorMenuItem::new();
-    menu.append(&separator);
-
-    /* -------------------------------------------------------------------------- */
-    /*                                    Quit                                    */
-    /* -------------------------------------------------------------------------- */
-    let quit = gtk::MenuItem::builder()
-        .label("Quit")
-        .accel_path("<MP3Player>/File/Quit")
-        .build();
-    quit.connect_activate(move |_| {
-        app.quit();
-    });
-    menu.append(&quit);
-    /* -------------------------------------------------------------------------- */
-    menu
+    app.add_action(&open_action);
+    let quit = gio::SimpleAction::new("quit", None);
+    quit.connect_activate(glib::clone!(@weak window => move |_, _| {
+        window.close();
+    }));
+    app.add_action(&quit);
 }
 
-/// Build the Edit menu
-fn build_edit_menu() -> gtk::Menu {
-    let menu = gtk::Menu::new();
-
-    /* -------------------------------------------------------------------------- */
-    /*                                  Preference                                */
-    /* -------------------------------------------------------------------------- */
-    let preferences = gtk::MenuItem::builder()
-        .label("Preferences")
-        .accel_path("<MP3Player>/Edit/Preferences")
+fn build_ui(app: &gtk::Application) {
+    let window: ApplicationWindow = ApplicationWindow::builder()
+        .application(app)
+        .title("MP3 Player")
+        .default_width(800)
+        .default_height(600)
         .build();
-    preferences.connect_activate(|_| {
-        println!("Preferences");
-    });
-    menu.append(&preferences);
-    /* -------------------------------------------------------------------------- */
-    menu
-}
+    window.set_show_menubar(true);
 
-fn build_help_menu() -> gtk::Menu {
-    let menu = gtk::Menu::new();
-    /* -------------------------------------------------------------------------- */
-    /*                                    About                                   */
-    /* -------------------------------------------------------------------------- */
-    let about = gtk::MenuItem::builder()
-        .label("About")
-        .accel_path("<MP3Player>/Help/About")
-        .build();
-    about.connect_activate(|_| {
-        println!("About");
-    });
-    menu.append(&about);
-    /* -------------------------------------------------------------------------- */
-    menu
-}
+    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    let label = gtk::Label::new(Some("MP3 Player"));
+    vbox.append(&label);
+    window.set_child(Some(&vbox));
 
-fn build_menu(app: Application) -> gtk::MenuBar {
-    let menu_bar = gtk::MenuBar::new();
+    build_menu(app);
 
-    /* -------------------------------------------------------------------------- */
-    /*                                  File Menu                                 */
-    /* -------------------------------------------------------------------------- */
-    let file_menu = build_file_menu(app);
-    let file_menu_item = gtk::MenuItem::builder()
-        .label("File")
-        .submenu(&file_menu)
-        .build();
-    menu_bar.append(&file_menu_item);
+    add_actions(app, &window);
 
-    /* -------------------------------------------------------------------------- */
-    /*                                  Edit Menu                                 */
-    /* -------------------------------------------------------------------------- */
-    let edit_menu = build_edit_menu();
-    let edit_menu_item = gtk::MenuItem::builder()
-        .label("Edit")
-        .submenu(&edit_menu)
-        .build();
-    menu_bar.append(&edit_menu_item);
-
-    /* -------------------------------------------------------------------------- */
-    /*                                  Help Menu                                 */
-    /* -------------------------------------------------------------------------- */
-    let help_menu = build_help_menu();
-    let help_menu_item = gtk::MenuItem::builder()
-        .label("Help")
-        .submenu(&help_menu)
-        .build();
-    menu_bar.append(&help_menu_item);
-    /* -------------------------------------------------------------------------- */
-    menu_bar
+    window.show();
 }
 
 fn main() {
     let application = Application::builder()
         .application_id("org.ncc.mp3player")
         .build();
-    application.connect_activate(|app| {
-        let window: ApplicationWindow = ApplicationWindow::builder()
-            .application(app)
-            .title("MP3 Player")
-            .default_width(800)
-            .default_height(600)
-            .build();
 
-        let header = HeaderBar::builder()
-            .title("MP3 Player")
-            .show_close_button(true)
-            .build();
-
-        let menu_bar = build_menu(app.clone());
-        header.pack_start(&menu_bar);
-        window.set_titlebar(Some(&header));
-        let main_box = gtk::Box::new(gtk::Orientation::Vertical, 0);
-        main_box.add(&gtk::Label::new(Some("Hello, world!")));
-        window.set_child(Some(&main_box));
-
-        window.show_all();
+    application.connect_startup(|app| {
+        add_accelerators(app);
     });
+
+    application.connect_activate(build_ui);
 
     application.run();
 }
