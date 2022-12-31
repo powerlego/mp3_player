fb8a6b9b1eb0c18b66b0695ab2f654eef30a4a0f
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2022-12-30 22:00:22 -0600

    Begun adding settings backend implementation

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 from PySide6 import QtWidgets
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QSettings
 from widgets import MainWindow
 
 def main():
     app = QApplication(sys.argv)
+    app.setApplicationName("MP3 Player")
+    QSettings.setDefaultFormat(QSettings.Format.IniFormat)
     window = MainWindow()
     window.show()
     app.exec()
--- widgets/settings_window.py
+++ widgets/settings_window.py
@@ -1,8 +1,19 @@
 from PySide6 import QtWidgets
 from PySide6.QtWidgets import QDialog
+from PySide6.QtCore import QSettings
 
 class SettingsWindow(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle("Settings")
         self.resize(400, 300)
+        self.setModal(True)
+        self.settings = QSettings()
+        self.theme = self.settings.value("theme", "dark")
+        self.language = self.settings.value("language", "en")
+        self.volume = self.settings.value("volume", 50)
+        
+    def write_settings(self):
+        self.settings.setValue("theme", self.theme)
+        self.settings.setValue("language", self.language)
+        self.settings.setValue("volume", self.volume)
